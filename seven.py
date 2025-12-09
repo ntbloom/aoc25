7f4c989e85553ab83cf306cdb025829d32ff52e8
Noah Bloom <nbloom@nrao.edu>
2025-12-09 00:21:15 -0500

    working through part 2, still pretty buggy

--- seven.py
+++ seven.py
@@ -11,9 +11,13 @@ class Node:
         self.row = row
         self.split = False
         self.visited = False
+        self.count = 0
 
     def print(self) -> None:
         rprint(f"{'[red]' if self.visited else '[green]'}{self.char}", end="")
+
+    def print_count(self) -> None:
+        rprint(f"[red]C" if self.count else f"[green]{self.char}", end="")
 
 
 class Graph:
@@ -43,6 +47,12 @@ class Graph:
                 node.print()
             print()
 
+    def print_count(self):
+        for row in self.grid:
+            for node in row:
+                node.print_count()
+            print()
+
     def tachyon(self, node: Node):
         if node.visited:
             return
@@ -61,6 +71,69 @@ class Graph:
                 self.tachyon(self.grid[node.row + 1][node.col + 1])
             case _:
                 raise ValueError(f"unexpected char: {node.char}")
+
+    def left_parent(self, node: Node) -> Node | None:
+        if node.col == 0:
+            return None
+        left = node.col - 1
+        found = False
+        idx = node.row - 1
+        while not found and idx != 0:
+            parent = self.grid[idx][left]
+            if parent.char == "^" and parent.visited:
+                return parent
+            idx -= 1
+        return None
+
+    def right_parent(self, node: Node) -> Node | None:
+        if node.col == self.width - 1:
+            return None
+        right = node.col + 1
+        found = False
+        idx = node.row - 1
+        while not found and idx != 0:
+            parent = self.grid[idx][right]
+            if parent.char == "^" and parent.visited:
+                return parent
+            idx -= 1
+        return None
+
+    def quantum(self) -> int:
+        assert self.start
+        self.tachyon(self.start)
+
+        self.start.count = 1
+        for idx, row in enumerate(self.grid):
+            if idx == 0:
+                continue
+            # mark first split manually
+            elif idx == 2:
+                for node in row:
+                    if node.char == "^":
+                        node.count = 1
+                continue
+            for node in row:
+                left_count = 0
+                right_count = 0
+                if node.char == "^" and node.visited:
+                    if left_up := self.left_parent(node):
+                        left_count = left_up.count
+                    if right_up := self.right_parent(node):
+                        right_count = right_up.count
+                node.count = left_count + right_count
+
+        total = 0
+        for node in self.grid[self.length]:
+            count = 0
+            if self.grid[self.length - 1][node.col].char == "^":
+                continue
+            if left := self.left_parent(node):
+                count += left.count
+            if right := self.right_parent(node):
+                count += right.count
+            node.count = count
+            total += node.count
+        return total
 
     def count_splits(self) -> int:
         assert self.start
@@ -81,6 +154,7 @@ def one() -> int:
 
 
 def two() -> int:
-    with open(get_path(DAY), "r") as f:
-        pass
-    return 0
+    g = Graph()
+    answer = g.quantum()
+    g.print_count()
+    return answer
